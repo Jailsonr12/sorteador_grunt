7cf5949c157b4d321d6016a543628598fa904bc7
Jailsonr12 <jailson.roth@outlook.com>
2024-12-29 17:05:27 -0300

    correcação caminho main.min.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function (grunt) {
                         },
                         {
                             match: "ENDERECO_DO_JS",
-                            replacement: "./src/scripts/main.min.js",
+                            replacement: "./scripty/main.min.js",
                         },
                     ],
                 },
